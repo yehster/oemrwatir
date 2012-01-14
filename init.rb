478b67c105484cb8e3f853bb6486c306b6a5bc39
Kevin Yeh <kevinyeh@alum.mit.edu>
2012-01-14 01:12:30 -0500

    timing

--- init.rb
+++ init.rb
@@ -4,3 +4,6 @@ require "./openemr_session.rb"
 $b=firebugBrowser()
 oemrLogin($b,$openemr,"admin","pass")
 $os=OpenemrSession.new $b
+$os.mainWindow.wait_until_present
+
+$os.selectPat "Doe"
--- openemr_session.rb
+++ openemr_session.rb
@@ -10,16 +10,22 @@ class OpenemrSession
     @brw.frame(:name,"RTop")
   end
   def nav()
-    ret=@brw.frame(:name,"left_nav")
+    @brw.frame(:name,"left_nav").wait_until_present
+    @brw.frame(:name,"left_nav")
   end
   def gotoNav(id)
-    nav().link(:id=>id).click
+      nav().link(:id=>id).click
   end
   def gotoSearch()
     gotoNav("new0")
   end
   def selectPat(name)
     gotoSearch()
+    sleep 0.5 until begin
+     mainWindow.text_field(:id=>'form_lname').exists?
+    rescue
+      p "Waiting"
+    end
     lname=mainWindow.text_field(:id=>'form_lname')
     lname.click
     lname.set name
@@ -27,7 +33,11 @@ class OpenemrSession
     mainWindow.button(:id=>'search').click
     @brw.window(:url=>/patient_select.php/).use
     @brw.div(:id=>"searchResults").table[1].click
-    sleep 2
-    @brw.windows[0].use    
+     sleep 1 until begin
+       @brw.windows[0].use
+     rescue 
+
+     end
+    
   end
 end
