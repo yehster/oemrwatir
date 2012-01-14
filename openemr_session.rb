14df0417f415cece8034df47ebd1ddd1f9f2b4a4
Kevin Yeh <kevinyeh@alum.mit.edu>
2012-01-14 00:07:26 -0500

    progress

--- openemr_session.rb
+++ openemr_session.rb
@@ -7,8 +7,27 @@ class OpenemrSession
     @brw = b
   end
   def mainWindow()
-    ret =@brw.frame(:name,"RTop")
-    ret.locate
-    return ret
+    @brw.frame(:name,"RTop")
+  end
+  def nav()
+    ret=@brw.frame(:name,"left_nav")
+  end
+  def gotoNav(id)
+    nav().link(:id=>id).click
+  end
+  def gotoSearch()
+    gotoNav("new0")
+  end
+  def selectPat(name)
+    gotoSearch()
+    lname=mainWindow.text_field(:id=>'form_lname')
+    lname.click
+    lname.set name
+    url=@brw.url
+    mainWindow.button(:id=>'search').click
+    @brw.window(:url=>/patient_select.php/).use
+    @brw.div(:id=>"searchResults").table[1].click
+    sleep 2
+    @brw.windows[0].use    
   end
 end
